06d1f9e02743cdc850e76964aa46601791789719
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-07-26 23:52:35 -0700

    Updated and better organized JS, multiple outline classes, new user-template

--- public/base.js
+++ public/base.js
@@ -1,3 +1,7 @@
+/**
+ * Define the permissions that this has overlay has access to,
+ * so that users know what this
+ */
 var slime2Setup = {
   permissions: ['chat'],
 }
@@ -18,73 +22,102 @@ var slime2Chat = {
     // get useful data from message
     const { parts, user } = message
 
-    // if the user hasn't set a name color,
-    // generate a color for them from the DEFAULT_USER_COLORS list
-    user.color ||= generateUserColor(user.displayName)
-
     // clone the main message template to insert the message data into
     const messageClone = cloneTemplate('message-template')
 
-    // go through every message part, inserting in the emote, cheer, or text data
-    const messageContent = messageClone.find('.content')
-    parts.forEach(part => {
-      switch (part.type) {
-        case 'emote': // insert in the emote image
-          {
-            const { image } = part.emote
-
-            const emoteClone = cloneTemplate('emote-template')
-            emoteClone.find('.emote').attr('src', image)
-
-            messageContent.append(emoteClone)
-          }
-          break
-
-        case 'cheer': // insert in the cheermote image and cheer amount
-          {
-            const { amount, color, image } = part.cheer
-
-            const cheerClone = cloneTemplate('cheermote-template')
-            cheerClone.find('.cheer').attr('src', image)
-            cheerClone.find('.cheer-amount').text(amount).css('color', color)
-
-            messageContent.append(cheerClone)
-          }
-          break
-
-        case 'text': // insert in the text
-        default:
-          {
-            const textClone = cloneTemplate('text-template')
-            textClone.find('.text').text(part.text)
-
-            messageContent.append(textClone)
-          }
-          break
-      }
-    })
-
-    // go through every visible user badge, inserting in the badge image
-    user.badges.forEach(badge => {
-      const badgeClone = cloneTemplate('badge-template')
-      badgeClone.find('.badge').attr('src', badge.image)
-      messageClone.find('.badges').append(badgeClone)
-    })
-
-    let name = user.displayName
-
-    // handle localized display names
-    // https://blog.twitch.tv/en/2016/08/22/localized-display-names-e00ee8d3250a/
-    if (user.displayName.toLowerCase() !== user.userName.toLowerCase()) {
-      name = `${user.displayName} (${user.userName})`
-    }
-
-    // insert in the user's pronouns, display name, and name color
-    messageClone.find('.pronouns').text(user.pronouns).css('color', user.color)
-    messageClone.find('.username').text(name).css('color', user.color)
+    messageClone.find('.user').append(buildUser(user))
+    messageClone.find('.content').append(buildContent(parts))
 
     return [messageClone]
   },
+}
+
+/********************\
+ * Element Builders *
+\********************/
+
+function buildUser(user) {
+  const { color, displayName, userName, badges, pronouns } = user
+
+  const userClone = cloneTemplate('user-template')
+  userClone.find('.badges').append(buildBadges(badges))
+
+  // if the user hasn't set a name color,
+  // generate a color for them from the DEFAULT_USER_COLORS list
+  const nameColor = color || generateUserColor(displayName)
+
+  let name = displayName
+
+  // handle localized display names
+  // https://blog.twitch.tv/en/2016/08/22/localized-display-names-e00ee8d3250a/
+  if (displayName.toLowerCase() !== userName.toLowerCase()) {
+    name = `${displayName} (${userName})`
+  }
+
+  // insert in the user's pronouns, display name, and name color
+  userClone.find('.pronouns').text(pronouns).css('color', nameColor)
+  userClone.find('.name').text(name).css('color', nameColor)
+
+  return userClone
+}
+
+// go through every message part, inserting in the emote, cheer, or text data
+function buildContent(parts) {
+  return parts.map(part => {
+    switch (part.type) {
+      case 'emote':
+        return buildEmote(part)
+
+      case 'cheer':
+        return buildCheer(part)
+
+      case 'text':
+      default:
+        return buildText(part)
+    }
+  })
+}
+
+function buildBadges(badges) {
+  return badges.map(badge => {
+    const { image } = badge
+
+    const badgeClone = cloneTemplate('badge-template')
+    badgeClone.find('.badge').attr('src', image)
+
+    return badgeClone
+  })
+}
+
+// insert in the emote image
+function buildEmote(part) {
+  const { images } = part.emote
+
+  const emoteClone = cloneTemplate('content-emote-template')
+  emoteClone.find('.emote').attr('src', images.default.x4)
+
+  return emoteClone
+}
+
+// insert in the cheermote image and cheer amount
+function buildCheer(part) {
+  const { amount, color, images } = part.cheer
+
+  const cheerClone = cloneTemplate('content-cheer-template')
+  cheerClone.find('.emote').attr('src', images.default.x4)
+  cheerClone.find('.cheer-amount').text(amount).css('color', color)
+
+  return cheerClone
+}
+
+// insert in the text
+function buildText(part) {
+  const { text } = part
+
+  const textClone = cloneTemplate('content-text-template')
+  textClone.find('.text').text(text)
+
+  return textClone
 }
 
 // generates a color from DEFAULT_USER_COLORS based on the name given,
