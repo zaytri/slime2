8e1dc2fac8282243a70b03ad237ff18874f93a99
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-07-27 00:05:17 -0700

    Change LOGIN.js to TOKEN.js and add scroll warning

--- public/LOGIN.js
+++
@@ -1,8 +0,0 @@
-var slime2Login = {
-  //! Do not share your token with anyone!
-  //! Theme Developers: Remember to remove your token before release!
-  // Go to https://slime.chat/token to get your token
-  twitch: {
-    chat: 'your token here',
-  },
-}
---
+++ public/TOKEN.js
@@ -0,0 +1,105 @@
+var slime2Login = {
+  //! Do not share your tokens with anyone!
+  //! Theme Developers: Remember to remove your tokens before release!
+
+  /**************************************************************************\
+   * Go to https://slime.chat/token to get your tokens
+   * Scroll down to view your tokens, this is for your own protection in case
+     you accidentally open this file in front of someone else or on stream
+  \**************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  twitch: {
+    chat: 'your token here',
+  },
+}
