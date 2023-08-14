574f590cd5776487eaf8cb3e11aa9e317e0a06ab
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-08-14 12:38:46 -0700

    Typo fix

--- public/base.js
+++ public/base.js
@@ -141,7 +141,7 @@ function cloneTemplate(id) {
   return $(document.getElementById(id).content.cloneNode(true))
 }
 
-// generated a random integer between min (included) and max (included)
+// generate a random integer between min (included) and max (included)
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
