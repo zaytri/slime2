3ea696de9520a980f599e52e0e19b0af3d99cf82
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-08-14 14:06:11 -0700

    Updated click test message styling

--- src/components/CursorControls.tsx
+++ src/components/CursorControls.tsx
@@ -9,6 +9,7 @@ type Position = {
 export default function CursorControls() {
   const [position, setPosition] = useState<Position>({ x: 0, y: 0 })
   const [visible, setVisible] = useState(false)
+  const [mouseDown, setMouseDown] = useState(false)
 
   const { sendTestMessage } = useTestMessage()
 
@@ -32,16 +33,28 @@ export default function CursorControls() {
       setVisible(false)
     }
 
+    function onMouseDown(_: MouseEvent) {
+      setMouseDown(true)
+    }
+
+    function onMouseUp(_: MouseEvent) {
+      setMouseDown(false)
+    }
+
     window.addEventListener('click', onClick)
     window.addEventListener('mousemove', onMouseMove)
     document.addEventListener('mouseenter', onMouseEnter)
     document.addEventListener('mouseleave', onMouseLeave)
+    document.addEventListener('mousedown', onMouseDown)
+    document.addEventListener('mouseup', onMouseUp)
 
     return () => {
       window.removeEventListener('click', onClick)
       window.removeEventListener('mousemove', onMouseMove)
       document.removeEventListener('mouseenter', onMouseEnter)
       document.removeEventListener('mouseleave', onMouseLeave)
+      document.removeEventListener('mousedown', onMouseDown)
+      document.removeEventListener('mouseup', onMouseUp)
     }
   }, [sendTestMessage])
 
@@ -50,21 +63,29 @@ export default function CursorControls() {
       style={{
         position: 'fixed',
         left: position.x,
-        width: 450,
+        width: 400,
         transform: 'translate(-50%, calc(-100% - 10px))',
         top: position.y,
         opacity: visible ? 1 : 0,
-        textAlign: 'center',
-        background: '#000A',
+        background: mouseDown ? '#0000008D' : '#0008',
+        border: mouseDown ? '8px inset #0008' : '8px outset #0008',
         color: 'white',
         fontFamily: 'sans-serif',
         borderRadius: 10,
-        padding: 20,
+        padding: 12,
         userSelect: 'none',
+        overflow: 'hidden',
         zIndex: 999,
+        textShadow:
+          '0 -1px black, 0 1px black, -1px 0 black, 1px 0 black, 0 -2px black, 0 2px black, -2px 0 black, 2px 0 black, -1px -1px black, 1px -1px black, -1px 1px black, 1px 1px black, -2px -2px rgba(0, 0, 0, 0.25), 2px -2px rgba(0, 0, 0, 0.25), -2px 2px rgba(0, 0, 0, 0.25), 2px 2px rgba(0, 0, 0, 0.25), 0 0 1px black, 0 0 2px black',
+        fontWeight: 'bold',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        boxSizing: 'border-box',
       }}
     >
-      Left Click to Send a Test Message
+      Click to Send a Test Message
     </div>
   )
 }
