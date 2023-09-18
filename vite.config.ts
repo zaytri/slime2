f6510a0a3e9132da11edcf197b0b7e5b414e7f05
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-09-18 02:00:56 -0700

    Automated release building and added HTML comments

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { version } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: { outDir: 'build' },
+  plugins: [
+    react(),
+    {
+      name: 'slime2-custom-index-transform',
+      transformIndexHtml: html => {
+        return html
+          .replace('<version>', version)
+          .replaceAll(
+            '/slime2',
+            `https://cdn.jsdelivr.net/gh/zaytri/slime2@${version}/release/slime2`,
+          )
+      },
+    },
+  ],
+  build: {
+    outDir: 'release',
+    assetsDir: '.',
+    rollupOptions: {
+      input: {
+        app: './base.html',
+      },
+      output: {
+        assetFileNames: 'slime2[extname]',
+        entryFileNames: 'slime2.js',
+      },
+    },
+  },
 })
