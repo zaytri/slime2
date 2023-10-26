4a05bccafe5c4c825ee25018cf195c220e8a644c
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-10-26 16:40:03 -0700

    merge pronouns.ts into usePronouns.ts

--- src/services/platforms/twitch/usePronouns.ts
+++ src/services/platforms/twitch/usePronouns.ts
@@ -1,6 +1,6 @@
 import { infiniteCache } from '@/services/settings'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
-import { getAllPronouns, getUserPronouns } from './pronouns'
+import axios from 'axios'
 
 export default function usePronouns() {
   const { data: pronounsMap } = useAllPronouns()
@@ -34,4 +34,41 @@ export function useAllPronouns() {
     },
     ...infiniteCache,
   })
+}
+
+const pronounsApi = axios.create({
+  baseURL: 'https://pronouns.alejo.io/api',
+})
+
+export async function getUserPronouns(
+  userName: string,
+): Promise<string | undefined> {
+  const data = await pronounsApi
+    .get<Pronouns.User[]>(`/users/${userName}`)
+    .then(response => response.data)
+    .catch(() => undefined)
+
+  if (!data) return undefined // if Alejo's server is down
+
+  // pronouns API returns either
+  // an array that contains a single User
+  // or an empty array if that user hasn't set pronouns
+  const [user] = data
+  if (!user) return undefined // no pronouns set
+
+  return user.pronoun_id
+}
+
+export async function getAllPronouns(): Promise<Map<string, string>> {
+  const pronounsMap = new Map<string, string>()
+
+  const allPronouns = await pronounsApi
+    .get<Pronouns.Data[]>('/pronouns')
+    .then(response => response.data)
+
+  allPronouns.forEach(({ name, display }) => {
+    pronounsMap.set(name, display)
+  })
+
+  return pronounsMap
 }
