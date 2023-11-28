4dd49372a7feba53f1474c88a31d04f21c184630
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-11-28 15:49:56 -0800

    finish DVD Bounce

--- widgets/dvd-bounce/dvd-bounce-script.js
+++ widgets/dvd-bounce/dvd-bounce-script.js
@@ -4,7 +4,6 @@ let currentMediaType = null
 // the slime2:ready event is fired once
 // indicates that the slime2 global variable is ready to use
 addEventListener('slime2:ready', () => {
-  slime2.widget.loadSettings('dvd-bounce-data.js', settings)
   run()
 })
 
@@ -14,14 +13,14 @@ addEventListener('slime2:widget-data-update', () => {
   // slime2.widget.getData() will always fetch the latest data
   const data = slime2.widget.getData()
 
-  const { width, height, opacity, mediaType, image, video, textBox } = data
+  const { opacity, mediaType, image, video, text } = data
 
   $('#dvd')
     .removeClass()
     .addClass(`media-${mediaType}`)
     .css({
-      '--width': `${width}px`,
-      '--height': `${height}px`,
+      '--imageScale': image.scale / 100,
+      '--videoScale': video.scale / 100,
       '--opacity': opacity / 100,
       '--image': `url('${image.url}')`,
     })
@@ -43,21 +42,24 @@ addEventListener('slime2:widget-data-update', () => {
     videoElement.load()
   }
 
-  $('#text-box').css({
-    alignItems: textBox.alignItems,
-    textAlign: textBox.textAlign,
-    fontFamily: textBox.font,
-    fontSize: textBox.fontSize,
-    fontWeight: textBox.fontWeight,
-    color: textBox.color,
-    backgroundColor: textBox.backgroundColor,
-    padding: textBox.padding,
-    borderRadius: textBox.borderRadius,
-    borderWidth: textBox.borderWidth,
-    borderColor: textBox.borderColor,
+  $('#text').css({
+    textAlign: text.textAlign,
+    fontFamily: text.font.name,
+    fontSize: text.font.size,
+    fontWeight: text.font.weight,
+    color: text.color,
+    backgroundColor: text.backgroundColor,
+    paddingTop: text.padding.top,
+    paddingBottom: text.padding.bottom,
+    paddingLeft: text.padding.left,
+    paddingRight: text.padding.right,
+    borderRadius: text.border.radius,
+    borderWidth: text.border.width,
+    borderColor: text.border.color,
+    borderStyle: text.border.style,
   })
 
-  $('#text').text(textBox.text)
+  $('#text').text(text.text)
 
   currentMediaType = mediaType
 })
@@ -81,8 +83,20 @@ function update(currentTimestamp) {
 }
 
 function move(timeElapsed) {
-  const { width, height, speed } = slime2.widget.getData()
-  if (!speed || !width || !height) return
+  const dvdElement = $('#dvd')
+  const { speed, mediaType, video, image } = slime2.widget.getData()
+  const elementWidth = dvdElement.width()
+  const elementHeight = dvdElement.height()
+
+  let scaleMultiplier = 1
+  if (mediaType === 'video') scaleMultiplier = video.scale / 100
+  if (mediaType === 'image') scaleMultiplier = image.scale / 100
+
+  // collision box for the text/image/video
+  const collisionWidth = elementWidth * scaleMultiplier
+  const collisionHeight = elementHeight * scaleMultiplier
+
+  if (!speed || !collisionWidth || !collisionHeight) return
 
   const pixelsMoved = (speed * timeElapsed) / 1000
   const boundingWidth = window.innerWidth
@@ -95,8 +109,8 @@ function move(timeElapsed) {
   }
 
   // collide right
-  if (position.x > boundingWidth - width) {
-    position.x = boundingWidth - width
+  if (position.x > boundingWidth - collisionWidth) {
+    position.x = boundingWidth - collisionWidth
     velocity.x = -1
   }
 
@@ -107,158 +121,15 @@ function move(timeElapsed) {
   }
 
   // collide bottom
-  if (position.y > boundingHeight - height) {
-    position.y = boundingHeight - height
+  if (position.y > boundingHeight - collisionHeight) {
+    position.y = boundingHeight - collisionHeight
     velocity.y = -1
   }
 
   position.x += velocity.x * pixelsMoved
   position.y += velocity.y * pixelsMoved
 
-  $('#dvd').css({
+  dvdElement.css({
     transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
   })
 }
-
-/************************
- * Settings Definitions *
- ************************/
-
-function defineSetting(label, id, type, options) {
-  return { label, id, type, ...options }
-}
-
-function defineSettingOption(label, value) {
-  return { label, value }
-}
-
-const settings = [
-  defineSetting(
-    `# DVD Bounce v1.0.0 by [Zaytri](https://zaytri.com/)
-Questions and Support: https://forums.slime2.stream/threads/20/`,
-    'title',
-    'text-display',
-  ),
-  defineSetting('Width (px)', 'width', 'number-input', {
-    defaultValue: 305,
-    slider: true,
-    min: 1,
-    max: 1000,
-    step: 1,
-  }),
-  defineSetting('Height (px)', 'height', 'number-input', {
-    defaultValue: 137,
-    slider: true,
-    min: 1,
-    max: 1000,
-    step: 1,
-  }),
-  defineSetting('Opacity (%)', 'opacity', 'number-input', {
-    defaultValue: 100,
-    slider: true,
-    min: 0,
-    max: 100,
-    step: 1,
-  }),
-  defineSetting('Speed (pixels per second)', 'speed', 'number-input', {
-    defaultValue: 100,
-    slider: true,
-    min: 1,
-    max: 1000,
-    step: 1,
-  }),
-  defineSetting('Media Type', 'mediaType', 'select-input', {
-    defaultValue: 'image',
-    options: [
-      defineSettingOption('Image', 'image'),
-      defineSettingOption('Video', 'video'),
-      defineSettingOption('Text Box', 'text'),
-    ],
-  }),
-  defineSetting('Image Settings', 'image', 'group', {
-    settings: [
-      defineSetting('Image', 'url', 'image-input', {
-        defaultValue: 'assets/dvd logo.png',
-      }),
-    ],
-  }),
-  defineSetting('Video Settings', 'video', 'group', {
-    settings: [
-      defineSetting('Video', 'url', 'video-input'),
-      defineSetting('Volume (%)', 'volume', 'number-input', {
-        defaultValue: 50,
-        slider: true,
-        min: 0,
-        max: 100,
-        step: 1,
-      }),
-    ],
-  }),
-  defineSetting('Text Box Settings', 'textBox', 'group', {
-    settings: [
-      defineSetting('Text', 'text', 'text-input', {
-        defaultValue: 'Your Text Here',
-        multiline: true,
-      }),
-      defineSetting('Text Vertical Align', 'alignItems', 'select-input', {
-        defaultValue: 'center',
-        options: [
-          defineSettingOption('Top', 'flex-start'),
-          defineSettingOption('Center', 'center'),
-          defineSettingOption('Bottom', 'flex-end'),
-        ],
-      }),
-      defineSetting('Text Horizontal Align', 'textAlign', 'select-input', {
-        defaultValue: 'center',
-        options: [
-          defineSettingOption('Left', 'left'),
-          defineSettingOption('Center', 'center'),
-          defineSettingOption('Right', 'right'),
-        ],
-      }),
-      defineSetting('Font', 'font', 'font-input'),
-      defineSetting('Font Size (px)', 'fontSize', 'number-input', {
-        defaultValue: 24,
-        min: 1,
-        step: 1,
-      }),
-      defineSetting('Font Weight', 'fontWeight', 'select-input', {
-        defaultValue: 'normal',
-        options: ['Normal', 'Bold']
-          .map(label => {
-            return defineSettingOption(label, label.toLowerCase())
-          })
-          .concat(
-            [100, 200, 300, 400, 500, 600, 700, 800, 900].map(value => {
-              return defineSettingOption(value.toString(), value)
-            }),
-          ),
-      }),
-
-      defineSetting('Text Color', 'color', 'color-input', {
-        defaultValue: 'white',
-      }),
-      defineSetting('Background Color', 'backgroundColor', 'color-input', {
-        defaultValue: 'black',
-      }),
-      defineSetting('Padding (px)', 'padding', 'number-input', {
-        defaultValue: 0,
-        min: 0,
-        step: 1,
-      }),
-      defineSetting('Border Radius (px)', 'borderRadius', 'number-input', {
-        defaultValue: 0,
-        min: 0,
-        step: 1,
-      }),
-      defineSetting('Border Width (px)', 'borderWidth', 'number-input', {
-        defaultValue: 0,
-        min: 0,
-        step: 1,
-      }),
-      defineSetting('Border Color', 'borderColor', 'color-input', {
-        defaultValue: 'transparent',
-      }),
-    ],
-  }),
-]
